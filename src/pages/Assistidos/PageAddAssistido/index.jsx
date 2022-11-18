0302ffa8fd973d7cc5bbb208269b881cd683ece7
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2022-11-18 17:05:29 -0300

    page addAssistido

--- src/pages/Assistidos/PageAddAssistido/index.jsx
+++ src/pages/Assistidos/PageAddAssistido/index.jsx
@@ -26,7 +26,7 @@ export function PageAddAssistido() {
   const verifyScrollSection = () => {
     if (
       window.scrollY <= identificacao.current.offsetTop ||
-      window.scrollY < endereco.current.offsetTop
+      window.scrollY < endereco.current.offsetTop - 80
     ) {
       setActiveIdentificacao("active");
     } else {
@@ -34,30 +34,30 @@ export function PageAddAssistido() {
     }
 
     if (
-      window.scrollY >= endereco.current.offsetTop &&
-      window.scrollY < condicoesSaude.current.offsetTop
+      window.scrollY >= endereco.current.offsetTop - 80 &&
+      window.scrollY < condicoesSaude.current.offsetTop - 80
     ) {
       setActiveEndereco("active");
     } else {
       setActiveEndereco("disable");
     }
     if (
-      window.scrollY >= condicoesSaude.current.offsetTop &&
-      window.scrollY < condicoesHabitacionais.current.offsetTop
+      window.scrollY >= condicoesSaude.current.offsetTop - 80 &&
+      window.scrollY < condicoesHabitacionais.current.offsetTop - 80
     ) {
       setActiveCondicoesSaude("active");
     } else {
       setActiveCondicoesSaude("disable");
     }
     if (
-      window.scrollY >= condicoesHabitacionais.current.offsetTop &&
-      window.scrollY < programasSociais.current.offsetTop
+      window.scrollY >= condicoesHabitacionais.current.offsetTop - 80 &&
+      window.scrollY < programasSociais.current.offsetTop - 80
     ) {
       setActiveCondicoesHabitacionais("active");
     } else {
       setActiveCondicoesHabitacionais("disable");
     }
-    if (window.scrollY >= programasSociais.current.offsetTop) {
+    if (window.scrollY >= programasSociais.current.offsetTop - 80) {
       setActiveProgramasSociais("active");
     } else {
       setActiveProgramasSociais("disable");
@@ -103,11 +103,172 @@ export function PageAddAssistido() {
           </a>
         </C.ContentInformations>
         <C.Contentregistration>
-          <div ref={identificacao}>1</div>
-          <div ref={endereco}>2</div>
-          <div ref={condicoesSaude}>3</div>
-          <div ref={condicoesHabitacionais}>4</div>
-          <div ref={programasSociais}>5</div>
+          <C.AreaIdentificacao ref={identificacao}>
+            <h2>Identificação</h2>
+            <fomr>
+              <C.InputColumn
+                style={{
+                  width: "350px",
+                }}
+              >
+                <label>Nome</label>
+                <C.Input type={"text"} required />
+              </C.InputColumn>
+
+              <C.InputColumn
+                style={{
+                  width: "350px",
+                }}
+              >
+                <label>Nome da mãe</label>
+                <C.Input type={"text"} />
+              </C.InputColumn>
+
+              <C.AreaInputsDisplayFlex
+                style={{
+                  gap: "1rem",
+                }}
+              >
+                <C.InputColumn
+                  style={{
+                    width: "150px",
+                  }}
+                >
+                  <label>Data nascimento</label>
+                  <C.Input type={"date"} required />
+                </C.InputColumn>
+
+                <C.InputColumn
+                  style={{
+                    width: "300px",
+                  }}
+                >
+                  <label>Nascido em</label>
+                  <C.Input type={"text"} />
+                </C.InputColumn>
+
+                <C.InputColumn
+                  style={{
+                    width: "105px",
+                  }}
+                >
+                  <label>Estado Civil</label>
+                  <C.Select>
+                    <option>Solteiro</option>
+                    <option>Casado</option>
+                    <option>Viúvo</option>
+                  </C.Select>
+                </C.InputColumn>
+              </C.AreaInputsDisplayFlex>
+
+              <C.AreaInputsDisplayFlex
+                style={{
+                  gap: "1rem",
+                }}
+              >
+                <C.InputColumn
+                  style={{
+                    width: "150px",
+                  }}
+                >
+                  <label>CPF</label>
+                  <C.Input type={"text"} required maxLength={11} />
+                </C.InputColumn>
+
+                <C.InputColumn
+                  style={{
+                    width: "180px",
+                  }}
+                >
+                  <label>RG</label>
+                  <C.Input type={"text"} maxLength={12} />
+                </C.InputColumn>
+              </C.AreaInputsDisplayFlex>
+            </fomr>
+          </C.AreaIdentificacao>
+
+          <C.AreaEndereco ref={endereco}>
+            <h2>Endereço</h2>
+            <fomr>
+              <C.InputColumn
+                style={{
+                  width: "450px",
+                }}
+              >
+                <label>Endereço</label>
+                <C.Input type={"text"} />
+              </C.InputColumn>
+
+              <C.AreaInputsDisplayFlex
+                style={{
+                  gap: "1rem",
+                }}
+              >
+                <C.InputColumn
+                  style={{
+                    width: "300px",
+                  }}
+                >
+                  <label>Bairro</label>
+                  <C.Input type={"text"} required />
+                </C.InputColumn>
+
+                <C.InputColumn
+                  style={{
+                    width: "300px",
+                  }}
+                >
+                  <label>Município</label>
+                  <C.Input type={"text"} />
+                </C.InputColumn>
+              </C.AreaInputsDisplayFlex>
+
+              <C.AreaInputsDisplayFlex
+                style={{
+                  gap: "1rem",
+                }}
+              >
+                <C.InputColumn
+                  style={{
+                    width: "200px",
+                  }}
+                >
+                  <label>Telefone de Contato</label>
+                  <C.Input type={"text"} required />
+                </C.InputColumn>
+
+                <C.InputColumn
+                  style={{
+                    width: "300px",
+                  }}
+                >
+                  <label>Localização</label>
+                  <C.Input type={"text"} />
+                </C.InputColumn>
+
+                <C.InputColumn
+                  style={{
+                    width: "105px",
+                  }}
+                >
+                  <label>Estado Civil</label>
+                  <C.Select>
+                    <option>Urbano</option>
+                    <option>Rural</option>
+                  </C.Select>
+                </C.InputColumn>
+              </C.AreaInputsDisplayFlex>
+            </fomr>
+          </C.AreaEndereco>
+          <C.AreaCondicoesSaude ref={condicoesSaude}>
+            <h2>Condições de Saúde</h2>
+          </C.AreaCondicoesSaude>
+          <C.AreaCondicoesHabitacionais ref={condicoesHabitacionais}>
+            <h2>Condições Habitacionais</h2>
+          </C.AreaCondicoesHabitacionais>
+          <C.ProgramasSociais ref={programasSociais}>
+            <h2>Acesso a Programas Sociais</h2>
+          </C.ProgramasSociais>
         </C.Contentregistration>
       </C.AreaContent>
     </C.Container>
--- src/pages/Assistidos/PageAddAssistido/styles.js
+++ src/pages/Assistidos/PageAddAssistido/styles.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 2rem 2rem 3rem 2rem;
+  padding: 2rem 6rem 3rem 6rem;
 `;
 
 export const AreaContent = styled.div`
   display: flex;
   margin: 2.5rem 0;
-
-  gap: 3rem;
 `;
 
 export const ContentInformations = styled.div`
@@ -38,16 +36,77 @@ export const ContentInformations = styled.div`
   }
 `;
 export const Contentregistration = styled.div`
-  margin-left: 350px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 360px;
   flex: 1;
+  gap: 2rem;
 
-  div {
-    height: 100vh;
-    border-radius: 5px;
-    background-color: red;
+  h2 {
+    margin-bottom: 2.5rem;
+  }
+`;
 
-    :nth-child(odd) {
-      background-color: blue;
+export const AreaIdentificacao = styled.div`
+  height: 80vh;
+  border-radius: 5px;
+  background: ${(props) => props.theme["gray-800"]};
+  padding: 2rem;
+`;
+export const AreaEndereco = styled.div`
+  height: 70vh;
+  border-radius: 5px;
+  background: ${(props) => props.theme["gray-800"]};
+  padding: 2rem;
+`;
+export const AreaCondicoesSaude = styled.div`
+  height: 80vh;
+  border-radius: 5px;
+  background: ${(props) => props.theme["gray-800"]};
+  padding: 2rem;
+`;
+export const AreaCondicoesHabitacionais = styled.div`
+  height: 80vh;
+  border-radius: 5px;
+  background: ${(props) => props.theme["gray-800"]};
+  padding: 2rem;
+`;
+export const ProgramasSociais = styled.div`
+  height: 80vh;
+  border-radius: 5px;
+  background: ${(props) => props.theme["gray-800"]};
+  padding: 2rem;
+`;
+
+export const AreaInputsDisplayFlex = styled.div`
+  display: flex;
+`;
+
+export const InputColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const Input = styled.input`
+  background: ${(props) => props.theme["gray-900"]};
+  border: none;
+  padding: 0.75rem 1rem;
+  color: ${(props) => props.theme["gray-300"]};
+  font-size: 0.9rem;
+
+  &[type="date"] {
+    ::-webkit-calendar-picker-indicator {
+      filter: invert(1);
     }
   }
+`;
+
+export const Select = styled.select`
+  background: ${(props) => props.theme["gray-900"]};
+  border: none;
+  padding: 0.75rem 1rem;
+  color: ${(props) => props.theme["gray-300"]};
+  font-size: 0.9rem;
 `;
